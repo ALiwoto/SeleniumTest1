afa28a0fe9399ec663c9e5b741753dec95900288
Aliwoto <woto@kaizoku.cyou>
2023-01-19 23:31:02 +0330

    Add project files.

---
+++ SeleniumTest1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTest1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var chromeOptions = new ChromeOptions();
+            //chromeOptions.AddArgument("--headless");
+
+            var driver = new ChromeDriver(chromeOptions);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(5000);
+
+            var nav = driver.Navigate();
+            
+            nav.GoToUrl("https://www.selenium.dev/selenium/web/web-form.html");
+
+            //var textBox = driver.FindElement(By.Name("my-text"));
+            var textBox = driver.FindElement(By.Id("my-text-id"));
+            var submitButton = driver.FindElement(By.TagName("button"));
+
+            var disabledInput = driver.FindElement(By.Name("my-disabled"));
+            if (disabledInput.GetAttribute("disabled") != "true")
+            {
+                disabledInput.SendKeys("test for disabled");
+            } 
+
+            var myClass = disabledInput.GetAttribute("class");
+
+            var myChecks = driver.FindElements(By.Name("my-check"));
+            foreach (var myCheck in myChecks )
+            {
+                if (myCheck.GetAttribute("checked") != "true")
+                {
+                    myCheck.Click();
+                }
+            }
+
+            var myColor = driver.FindElement(By.Name("my-colors"));
+
+            var theColorValue = myColor.GetAttribute("value");
+
+            var result = driver.ExecuteScript($"document.getElementsByName('{myColor.GetAttribute("name")}')[0].value='#FFC0CB'");
+
+            textBox.SendKeys("Selenium");
+            //submitButton.Click();
+
+            Console.WriteLine("Hello, World!");
+
+            driver.Quit();
+        }
+    }
+}

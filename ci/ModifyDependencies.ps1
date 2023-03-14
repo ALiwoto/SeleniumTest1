cb67b71dceeeb312a227b861caeca51199b4113e
Aliwoto <woto@kaizoku.cyou>
2023-03-14 04:27:54 +0330

    Add ModifyDependencies script file and fix minor issues at github workflow file.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

---
+++ ci/ModifyDependencies.ps1
@@ -0,0 +1,30 @@
+# ModifyDependencies.ps1 script.
+# This script file is responsible for modifying versions
+# (and other attributes of dependencies defined in different
+# files of the project).
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $false)]
+    [string]$CsProjPath = ".\SeleniumTest1\SeleniumTest1.csproj"
+)
+
+# Make sure System.Xml.Linq is loaded before proceeding.
+[System.Reflection.Assembly]::LoadWithPartialName("System.Xml.Linq")
+
+$csProjContent = Get-Content -Path $CsProjPath -Raw
+
+$myDocX = [System.Xml.Linq.XDocument]::Parse($csProjContent)
+
+$packageReferences = $myDocX.Element("Project").Elements("ItemGroup").Elements("PackageReference")
+[System.Xml.Linq.XElement]$chromeDriver = $packageReferences | Where-Object {
+    $_.Attribute("Include").Value -eq "Selenium.WebDriver.ChromeDriver" 
+}
+
+# change version of chromeDriver package to the one that works.
+if ($chromeDriver.Attribute("Version").Value -ne "110.0.5481.7700") {
+    $chromeDriver.SetAttributeValue("Version", "110.0.5481.7700")
+}
+
+$finalValue = $myDocX.ToString()
+Set-Content -Path $CsProjPath -Value $finalValue -Force
